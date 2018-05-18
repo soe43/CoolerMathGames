86ff12549e8357ad182a5eb63024ef2c213b0ae6
soe43 <soe433@gmail.com>
2018-05-18 14:01:55 -0400

    added tank.js and edited game.html

---
+++ static/js/tank.js
@@ -0,0 +1,14 @@
+var svg = document.getElementById("svg");
+var ns = "http://www.w3.org/2000/svg";
+
+var drawTank = function(id, x, y, orientation){
+    var rect = document.createElementNS(ns, "rect");
+    rect.setAttribute("x", x - 25);
+    rect.setAttribute("y", y - 10);
+    rect.setAttribute("width", 50);
+    rect.setAttribute("height", 20);
+    rect.setAttribute("rx", 5);
+    rect.setAttribute("ry", 5);
+    rect.setAttribute("orientation",
+    svg.appendChild(rect);
+}
