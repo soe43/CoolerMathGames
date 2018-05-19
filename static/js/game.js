13b64299c519d781c3a54fe6ff357e69f760082e
Hyhyme <eluo1@stuy.edu>
2018-05-19 12:10:56 -0400

    pushed ranLine changes

--- static/js/game.js
+++ static/js/game.js
@@ -1,10 +1,4 @@
-var svg = document.getElementById("cvs");
-
-var start = false
-var x1;
-var x2;
-var y1;
-var y2;
+var svg = document.getElementById("svg");
 
 var makeLine = function( x1, x2, y1, y2 ) {
     var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
@@ -17,27 +11,30 @@ var makeLine = function( x1, x2, y1, y2 ) {
 }
 
 var makeRanLine = function() {
-    if( start == false ) {
-	y1 = Math.random()*(200) + 300;
-	x1 = 0;
-    }
-    while( x < 500 ) {
-	changeX = Math.random()*(50);
-	changeY = Math.random()*(50);
-	if( x2 < 300 || x2 > 500 ) {
-	    
-	    
+    var y1 = Math.random()*(200) + 300;
+    var x1 = 0;
+    var x2 = 0;
+    var y2 = y1;
+    
+    while( x1 < 500 ) {
+	var changeX = Math.random()*(100);
+	var changeY = Math.random()*(100) - 50;
 	
-	makeLine( x1, x2, y1, y2 );
+	if( y2 + changeY < 300 || y2 + changeY > 500 ) {
+	    y2 = y2 - changeY;
+	}	    
+
+	x2 = x2 + changeX;
 	
+	makeLine( x1, x2, y1, y2 );	
 	x1 = x2;
 	y1 = y2;
+	console.log( 'x1: ' + x1 );
+	console.log( 'x2: ' + x2 );
+	console.log( 'y1: ' + y1 );
+	console.log( 'y2: ' + y2 );
     }
 }
 
-var makeRanFloor = function() {
-    while( x < 500 ) {
-	
-
-makeLine( 0, 500, 400, 400 );
+//makeLine( 0, 500, 400, 400 );
     
