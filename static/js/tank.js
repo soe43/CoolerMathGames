ae03b4b7e62d218eea73c06050131c1a6ea3d5c8
soe43 <soe433@gmail.com>
2018-05-20 01:08:24 -0400

    modified tank.js

--- static/js/tank.js
+++ static/js/tank.js
@@ -3,12 +3,27 @@ var ns = "http://www.w3.org/2000/svg";
 
 var drawTank = function(id, x, y, orientation){
     var rect = document.createElementNS(ns, "rect");
-    rect.setAttribute("x", x - 25);
-    rect.setAttribute("y", y - 10);
+    var cx = x-25;
+    var cy = y-10;
+    rect.setAttribute("x", cx);
+    rect.setAttribute("y", cy);
     rect.setAttribute("width", 50);
     rect.setAttribute("height", 20);
     rect.setAttribute("rx", 5);
     rect.setAttribute("ry", 5);
     rect.setAttribute("orientation", orientation);
+    rect.setAttribute("class", "tank" + id);
     svg.appendChild(rect);
+    var circ1 = document.createElementNS(ns, "circle");
+    circ1.setAttribute("cx", cx + 10);
+    circ1.setAttribute("cy", cy + 15);
+    circ1.setAttribute("r", 10);
+    circ1.setAttribute("class", "tank" + id);
+    svg.appendChild(circ1);
+    var circ2 = document.createElementNS(ns, "circle");
+    circ2.setAttribute("cx", cx + 40);
+    circ2.setAttribute("cy", cy + 15);
+    circ2.setAttribute("r", 10);
+    circ2.setAttribute("class", "tank" + id);
+    svg.appendChild(circ2);
 }
