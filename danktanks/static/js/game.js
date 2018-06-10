2b3dc92b10c8f0bf904a54d2353b2c080baf670b
xchen5 <xchen5@stuy.edu>
2018-06-10 15:37:13 -0400

    appended button + changed gravity function to include different tanks

--- danktanks/static/js/game.js
+++ danktanks/static/js/game.js
@@ -24,13 +24,13 @@ var makeRanLine = function() {
     var floorPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
     var floorPts = 'M0 '+ y2;
 
-    
+
     while( x1 < svgWidth ) {
 	var changeX = Math.random()*(100) + 10;
 	var changeY = Math.random()*(100) - 50;
 
 	floorPts += 'L' + x2 + ' ' + y2 + ' ';
-	
+
 	if( y2 + changeY <= svgHeight / 2 || y2 + changeY >= svgHeight ) {
 	    y2 = y2 - changeY;
 	} else {
@@ -40,7 +40,7 @@ var makeRanLine = function() {
 
 	//OLD WAY WE MADE GROUND. OBSOLETE
 	/*makeLine( x1, x2, y1, y2 );
-	
+
 	//fills space under ground by making trapezoids
 	var pts = '' + x1 + ',' + y1 + ' ' + x2 + ',' + y2 + ' ' + x2 + ',' + svgHeight + ' ' + x1 + ',' + svgHeight;
 	var trap = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
@@ -50,12 +50,12 @@ var makeRanLine = function() {
 
 	//hides weird vertical white lines a little
 	makeLine( x2, x2, y2, svgHeight );*/
-	
+
 	x1 = x2;
 	y1 = y2;
 
     }
-    //makes floor single path	
+    //makes floor single path
     floorPts += 'L' + x2 + ' ' + y2 + ' ';
     floorPts += 'L' + svgWidth + ' ' + svgHeight + ' L0 ' + svgHeight + ' Z';
     floorPath.setAttribute( 'd', floorPts );
@@ -105,23 +105,23 @@ var createButton = function(){
     var xlinkNS="http://www.w3.org/1999/xlink";
     a.setAttributeNS(xlinkNS,"href","/home/");
     a.appendChild(rect);
-    svg.appendChild(a);
     var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
     text.setAttribute('x', svgWidth - 95);
     text.setAttribute('y', 50);
     text.setAttribute('font-size','18');
     text.innerHTML = "Lobby";
-    svg.appendChild(text);
+    a.appendChild(text);
+    svg.appendChild(a);
 }
 /*var intersectRect = function(r1, r2) {
     var r1 = r1.getBBox();    //BOUNDING BOX OF THE FIRST OBJECT
     var r2 = r2.getBBox();    //BOUNDING BOX OF THE SECOND OBJECT
 
     console.log(r1.left);
-    
+
     //CHECK IF THE TWO BOUNDING BOXES OVERLAP
-  return !(r2.left > r1.right || 
-           r2.right < r1.left || 
+  return !(r2.left > r1.right ||
+           r2.right < r1.left ||
            r2.top > r1.bottom ||
            r2.bottom < r1.top);
 }
@@ -135,12 +135,12 @@ var gravity = function(){
 }
 
 setInterval(gravity, 30);
-    
+
 */
-var moveByVelocity = function(){
+var moveByVelocity = function(tankid){
     var terrain = document.getElementsByClassName("testPath")[0];
     var accelConst = 1.009;
-    var tanks = document.getElementsByClassName("tank0");
+    var tanks = document.getElementsByClassName("tank" + tankid);
     if (!svg.checkIntersection(tanks[0], terrain.getBBox())){
 	for(var i = 0; i < tanks.length; i++){
 	    if((tanks[i].getAttribute("vy") < 0)){
@@ -150,31 +150,35 @@ var moveByVelocity = function(){
 		    tanks[i].setAttribute("cy", parseFloat(tanks[i].getAttribute("cy")) + parseFloat(tanks[i].getAttribute("vy")) * -1);
 		}
 		else{
-		    tanks[i].setAttribute("vy", parseFloat(tanks[i].getAttribute("vy")) * accelConst); 
+		    tanks[i].setAttribute("vy", parseFloat(tanks[i].getAttribute("vy")) * accelConst);
 		    tanks[i].setAttribute("y", parseFloat(tanks[i].getAttribute("y")) + parseFloat(tanks[i].getAttribute("vy")) * -1);
 		}
 	    }
-	    
+
 	}
     }
-    
+
 }
-var gravity = function(){
-    
+var gravity = function(tankid){
+
     var terrain = document.getElementsByClassName("testPath")[0];
-    var tanks = document.getElementsByClassName("tank0");
+    var tanks = document.getElementsByClassName("tank" + tankid);
     if (!svg.checkIntersection(tanks[0], terrain.getBBox())){
 	for(var i = 0; i < tanks.length; i++){
 	    //console.log(tanks[i].getAttribute("vy") == null);
 	    if(tanks[i].getAttribute("vy") == null){
-		tanks[i].setAttribute("vy", -0.1);		
+		tanks[i].setAttribute("vy", -0.1);
 	    }
 	}
     }
-}   
-	    
+}
+
 
 testFloor();
 drawTank(0,250,250,0,"blue");
-setInterval(gravity, 10);
-setInterval(moveByVelocity, 10);
+drawTank(1,svgWidth - 250,250,180,"red");
+createButton();
+setInterval(gravity, 10, 0);
+setInterval(moveByVelocity, 10, 0);
+setInterval(gravity, 10, 1);
+setInterval(moveByVelocity, 10, 1);
